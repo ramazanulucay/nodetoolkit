47304f8d8585b457c89e9ce367dda344930a8922
Ramazan Uluçay <rulucay@atel.com.tr>
2019-08-21 15:57:14 +0300

    Nodejs Typescript için seed proje yüklendi.

---
+++ src/__tests__/Sum.test.ts
@@ -0,0 +1,4 @@
+import { Sum } from '../index';
+test('Toplama İşlemi Fonksiyonu', () => {
+  expect(Sum(1,2)).toBe(3);
+});
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export const Sum = (number1: number, number2: number) => number1+number2;
